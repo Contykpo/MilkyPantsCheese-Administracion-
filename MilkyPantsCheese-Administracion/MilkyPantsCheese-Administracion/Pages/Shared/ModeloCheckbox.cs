f8b3e40a150e34047ae273ab1d653134b60882dd
Crazy474747 <58375872+Crazy474747@users.noreply.github.com>
2021-11-29 15:22:19 -0300

    Ver descripcion
    -Añadido estilo a las checkboxes
    -Añadido estilo a los input de archivos

---
+++ MilkyPantsCheese-Administracion/MilkyPantsCheese-Administracion/Pages/Shared/ModeloCheckbox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Html;
+
+namespace MilkyPantsCheese.Pages
+{
+	/// <summary>
+	/// Modelo para las partial view de checkbox
+	/// </summary>
+	public record ModeloCheckbox
+	{
+		/// <summary>
+		/// Nombre de la propiedad a la que esta atado el checkbox
+		/// </summary>
+		public string nombrePropiedad;
+
+		/// <summary>
+		/// Contenido de la etiqueta del cehckbox
+		/// </summary>
+		public string contenido;
+
+		/// <summary>
+		/// Atributos que añadir al checkbox
+		/// </summary>
+		public object atributos;
+
+		/// <summary>
+		/// Checkbox
+		/// </summary>
+		public IHtmlContent checkBox;
+	}
+}
