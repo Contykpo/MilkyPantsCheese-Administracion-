7b8ce1d1dfdb699ad479314ded0941d28bf7e63f
ElGranNachito <58375872+ElGranNachito@users.noreply.github.com>
2021-12-02 20:15:02 -0300

    Ver descripcion
    -Avances con el historial de los datos de curado

--- MilkyPantsCheese-Administracion/MilkyPantsCheese-Administracion/Pages/Monitoreo/MonitoreoCurado.cshtml.cs
+++ MilkyPantsCheese-Administracion/MilkyPantsCheese-Administracion/Pages/Monitoreo/MonitoreoCurado.cshtml.cs
@@ -1,6 +1,13 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace MilkyPantsCheese.Pages
 {
@@ -8,9 +15,66 @@ namespace MilkyPantsCheese.Pages
     [Authorize(Roles = Constantes.NombreRolAdministrador + "," + Constantes.NombreRolCheeseScientist + "," + Constantes.NombreRolSusurradorDeQuesos)]
     public class MonitoreoCuradoModel : PageModel
     {
+        public readonly MilkyDbContext _dbContext;
+
+        public MonitoreoCuradoModel(MilkyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [BindProperty]
+        public bool UtilizarFiltradoPorIntervalo { get; set; }
+
+        [BindProperty]
+        [DisplayName("Tiempo entre los datos")]
+        public TimeSpan Intervalo { get; set; }
+
+        public List<ModeloDatosSensorCurado> HistorialDatos { get; set; } = new List<ModeloDatosSensorCurado>();
+
         public PartialViewResult OnGetActualizar()
         {
             return Partial("_DatosMonitoreoCurado");
+        }
+
+        public async Task<PartialViewResult> OnPostActualizarHistorial()
+        {
+            if(UtilizarFiltradoPorIntervalo)
+            {
+                var historial = await _dbContext.DatosSensorCurado.OrderByDescending(d => d.Fecha).ToListAsync();               
+
+                if (historial.Count > 1)
+                {
+                    historial.RemoveAt(0);
+                }
+
+                var historialFinal = new List<ModeloDatosSensorCurado>();
+
+                foreach (var entradaActual in historial)
+                {
+                    if(historialFinal.Count == 0)
+                    {
+                        historialFinal.Add(entradaActual);
+                    }
+                    else
+                    {
+                        if ((historialFinal.Last().Fecha - entradaActual.Fecha) >= Intervalo)
+                            historialFinal.Add(entradaActual);
+                    }
+                }
+
+                HistorialDatos = historialFinal;
+            }
+            else
+            {
+                HistorialDatos = await _dbContext.DatosSensorCurado.OrderByDescending(d => d.Fecha).ToListAsync();
+
+                if (HistorialDatos.Count > 1)
+                {
+                    HistorialDatos.RemoveAt(0);
+                }
+            }
+
+            return Partial("_HistorialDatosSensor", this);
         }
     }
 }
