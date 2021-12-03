66d7fba0c6efe09e47db74d89c56b7a09e59b5b0
Contykpo <samu.1furlanich@gmail.com>
2021-12-03 10:19:48 -0300

    Descripcion
    - Nueva migracion.

--- MilkyPantsCheese-Administracion/MilkyPantsCheese-Administracion/Migrations/20211203030043_Inicial.cs
+++ MilkyPantsCheese-Administracion/MilkyPantsCheese-Administracion/Migrations/20211203030043_Inicial.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace MilkyPantsCheese.Migrations
 {
-    public partial class inicial : Migration
+    public partial class Inicial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
